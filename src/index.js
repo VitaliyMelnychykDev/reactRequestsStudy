c67f735c206f40b805b78b55736759e07ee18c1a
Vitalii <vitaliymelnychukdev@gmali.com>
2018-01-23 12:18:51 +0200

    set baseURL config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+
 axios.interceptors.request.use(request => {
     console.log(request);
 
@@ -21,7 +23,7 @@ axios.interceptors.response.use(response => {
     return response;
 }, error => {
     console.log(error);
-    
+
     return Promise.reject(error);
 });
 
